2cad8f7006e561fe21f6a76a694cac9baeb125c7
Lucas Dantas <lucas.dantas@ifrn.edu.br>
2024-09-18 14:11:24 -0300

    Calcular idade a partir da data de nascimento

--- app/models/Pessoa.java
+++ app/models/Pessoa.java
@@ -1,6 +1,13 @@
 package models;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
 
 import play.db.jpa.Model;
 
@@ -9,6 +16,24 @@ public class Pessoa extends Model {
 
 	public String nome;
 	public String email;
+
+	// @Enumerated(EnumType.STRING)
+	// public Status status;
 	
+	@Temporal(TemporalType.DATE)
+	public Date dataNascimento;
+
+	@Transient
+	public Long idade;
+	
+	public Long getIdade() {
+		Date d1 = this.dataNascimento;
+		Date d2 = new Date();
+		
+		long differenceInTime = d2.getTime() - d1.getTime();
+		this.idade = (differenceInTime / (1000l * 60 * 60 * 24 * 365));
+		
+		return this.idade;
+	}
 }
 
